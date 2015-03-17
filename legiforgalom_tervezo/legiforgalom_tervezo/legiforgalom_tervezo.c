9bcfd16c6194b7b38ddd7bbb9420ac09ce8356f1
Asbóth Dániel <asboth.daniel@gmail.com>
2015-03-17 08:44:51 +0100

    Járatkeresés
    Megoldottam a járatszám keresést, és a menü megfelelő helyére
    beillesztettem !
    
    getline és egesze függvény beillesztve a bekéréshez illetve az
    ellenőrzéshez

--- legiforgalom_tervezo/legiforgalom_tervezo/legiforgalom_tervezo.c
+++ legiforgalom_tervezo/legiforgalom_tervezo/legiforgalom_tervezo.c
@@ -5,6 +5,7 @@
 
 #define MAX 128
 #define ADATOK "repulo.csv"
+#define HSZ sizeof(int)/sizeof(short)*5  /* 5 v 10 jegyu max az int meretetol fuggoen */
 
 typedef struct Data {
 	int jaratszam;
@@ -18,6 +19,43 @@ typedef struct Data {
 
 	struct Data *kov;
 }Data;
+
+int getline(char s[], int lim)
+{
+	int i;
+	char c;
+
+	for (i = 0; i<lim && (c = getchar()) != EOF && c != '\n'; ++i)
+		s[i] = c;
+
+	s[i] = '\0';
+
+	while (c != EOF && c != '\n')
+		c = getchar();
+	return i;
+
+}
+
+int egesze(char s[])
+{
+	int i = 0;
+	int kezd;
+
+	/* feher karakterek atlepese */
+	while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
+		i++;
+	if (s[i] == '+' || s[i] == '-')
+		i++;
+	kezd = i;
+
+	while (s[i] >= '0' && s[i] <= '9' && i - kezd<HSZ)
+		++i;
+
+	if (s[i] != 0 && s[i] != '\n' && s[i] != '\t' && s[i] != ' ')
+		return 0;
+	else
+		return 1;
+}
 
 void fajlellenorzes(){										
 	printf("Fájl ellenõrzése...\n");
@@ -57,7 +95,7 @@ Data *listaletrehoz(){
 
 		i = 0;
 		running = (seged);
-		for (p = strsep(running, ";"); p != NULL; p = strsep(NULL, ";"))											//strtok: tokenek alapján "szétdarabolja" a 
+		for (p = strsep(running, ";"); p != NULL; p = strsep(NULL, ";"))					//strtok: tokenek alapján "szétdarabolja" a 
 		{																					//stringet és visszatér a token helyének 
 			strcpy(seged2, p);																//pointerével
 
@@ -105,9 +143,34 @@ void listakiir(Data *lista){
 	printf("\n");
 }
 
-void set_charset(){						//Ékezetes karakterek hálleluja
+void set_charset(){									//Ékezetes karakterek hálleluja
 	system("chcp 1250");
 	system("cls");
+}
+
+void jaratszamkereses(Data *lista)					//Keressük meg a járatunkat
+{
+	Data *iter;
+	char seged[10];
+	int jaratszam = 0;
+
+	do{
+		printf("Kerem adja meg a jaratszamot: "); getline(seged, 9);	//Bekérjük a számot, addig amíg nem egész
+		if (egesze(seged) != 1) printf("Hibás járatszám ! Kérem adja meg újra !\n\n");
+	} while (egesze(seged) != 1);
+
+	jaratszam = atoi(seged);											//Átalakítom int-re
+
+	for (iter = lista; iter != NULL; iter = iter->kov){					//Végigfutok a listán és kiírok
+		if (iter->jaratszam == jaratszam){
+			printf("Járatszám: [%d]\n", iter->jaratszam);
+			printf("Honnan: [%s]\n", iter->repter1);
+			printf("Hová: [%s]\n", iter->repter2);
+			printf("Távolság: [%.2f km]\n", iter->tavolsag);
+			printf("Osztályok: [%s]\n\n", iter->osztalyok);
+		}
+	}
+
 }
 
 void main()
@@ -131,10 +194,12 @@ void main()
 			listakiir(lis);
 			break;
 		case 2:
+			
 			break;
 		case 3:
 			break;
 		case 4:
+			jaratszamkereses(lis);
 			break;
 		case 5:
 			exit(0);
