80c4882e4c6d3fb7b8b8a06c67d7cc84130b1486
Urbán Tibor Erik <uerik26@gmail.com>
2015-04-24 16:10:40 +0200

    NEW REVENGE!
    Új Struktúra
    Közvetlenül közlekedő járatok keresése működik, tesztelni kell minél
    többet.

--- legiforgalom_tervezo/legiforgalom_tervezo/legiforgalom_tervezo.c
+++ legiforgalom_tervezo/legiforgalom_tervezo/legiforgalom_tervezo.c
@@ -26,6 +26,41 @@ typedef struct Data {
 	struct Data *kov;
 }Data;
 
+//Állomás
+typedef struct csucs { // a gráf egy csúcsánhoz tartozó információk 
+	int id;     // csucs azonosito sorszama
+	char *allomas;
+	char country[MAX];
+	int parent_id;   // a csucs ose 
+}csucs;
+
+//Járat
+typedef struct el { // a gráf egy éléhez tartozó információk 
+	int el_id;
+	csucs kezdet;    // graf-el egyik vege 
+	csucs veg;    // graf-el masik vege
+	struct tm indulas;
+	struct tm erkezes;
+	int idoeltolodas;
+	double tavolsag;
+	char osztalyok[10];
+}el;
+
+//Egész hálózat
+typedef struct graf { // a teljes gráf 
+	csucs csucsok[35];
+	csucs szomszedosCsucsok[35][2];
+	el elek[MAX];
+}graf;
+
+//Útvonal hálózat
+typedef	struct utvonal{
+	csucs allomas;
+	int parent;
+	int el_id;
+	struct utvonal *kov;
+}utvonal;
+
 //Ékezetes betûk
 void set_charset(){									//Ékezetes karakterek hálleluja
 	system("chcp 1250");
@@ -513,148 +548,378 @@ void idopontkereses(Data *lista)
 	}
 }
 
-//Do magic
-char * utvonal(Data *lista, char *honnan, char *cel, char * utvonal_tomb, int *lvl, char * utvonal_end)
+/*
+char * utvonal(Data *lista, char *honnan, char *hova, int melyseg, char *parent)
 {
-	Data *iter, *iter2;
-	int i,j = 0;
-	char *tmp[MAX];
-	char *tok = ";";
-	int  words[MAX];
-	char  buff[MAX];
-	int ntokens = 0;
-	int szint = *lvl;
-	char * utvonal_p;
+	Data *iter;
+	char utvonal_tomb[MAX][MAX];
+	int index = 0;
+	int i = 0;
+	int index2 = 0;
+	int j = 0;
+	char * temp_utvonal ="";
+	int endindex = 0;
+	char temp[MAX];
+	char *buff = "";
+	char possible[MAX][MAX];
 
-	char  *sep = strtok(utvonal_tomb, ";");
 
-	if (strcmp(kisbetusito(honnan), kisbetusito(cel)) == 0)
+
+	for (iter = lista; iter != NULL; iter = iter->kov)
 	{
-		return utvonal_tomb;
+		if (strcmp(kisbetusito(honnan), kisbetusito(iter->repter1)) == 0){
+			itoa(iter->jaratszam, utvonal_tomb[index], 10);
+			index++;
+		}
 	}
 
-	while (sep != NULL)
+	for (iter = lista; iter != NULL; iter = iter->kov)
 	{
-		words[ntokens++] = atoi(sep);
-		sep = strtok(NULL, ";");
-	}
-
-
-
-	for (i = 0; i < MAX; i++)
-	{
-		for (iter = lista; iter != NULL; iter = iter->kov)
-		{
-			if (words[ntokens-1] == iter->jaratszam)
-			{
-				for (iter2 = lista; iter2 != NULL; iter2 = iter2->kov)
+		for (i = 0; i < index; i++){
+			if (iter->jaratszam == atoi(utvonal_tomb[i])){
+				if (strcmp(kisbetusito(hova), kisbetusito(iter->repter2)) == 0){
+					itoa(iter->jaratszam, utvonal_tomb[index2], 10);
+					index2++;
+					temp_utvonal = concat(temp_utvonal, utvonal_tomb[index2-1]);
+					temp_utvonal = concat(temp_utvonal, ";");
+					//temp_utvonal = concat(temp_utvonal, parent);
+					return temp_utvonal;
+					
+				}
+				else
 				{
 
-					if (array_contain(iter2->jaratszam, words,ntokens) == 0)
-					{
-						
-						if (strcmp(kisbetusito(iter2->repter1), kisbetusito(iter->repter2)) == 0)
-						{
-							itoa(iter2->jaratszam, tmp, 10);
-							for (j = 0; j < ntokens; j++)
-							{
-								itoa(words[j], utvonal_tomb, 10);
-								utvonal_tomb = concat(utvonal_tomb, ";");
-							}
-							utvonal_tomb = concat(utvonal_tomb, tmp);
-
-							if (strcmp(kisbetusito(iter2->repter2), kisbetusito(cel)) == 0){
-								return utvonal_tomb;
-								for (utvonal_p = strsep(utvonal_tomb, ";"); utvonal_p != NULL; utvonal_p = strsep(NULL, ";"))
-								{
-									if (strcmp(utvonal_p, utvonal_tomb) != 0)
-									{
-										strcpy(utvonal_end, utvonal_tomb);
-										return utvonal_tomb;
-									}
-								}
-							}
-							else
-							{
-								if (szint == 0)
-								{
-									return utvonal_tomb;
-									for (utvonal_p = strsep(utvonal_tomb, ";"); utvonal_p != NULL; utvonal_p = strsep(NULL, ";"))
-									{
-										if (strcmp(utvonal_p, utvonal_tomb) != 0)
-										{
-											strcpy(utvonal_end, utvonal_tomb);
-											return utvonal_tomb;
-										}
-									}
-								}
-								szint=szint-1;
-								strcpy(buff,utvonal(lista, honnan, cel, utvonal_tomb,&szint, utvonal_end));
-								utvonal_tomb = concat(utvonal_tomb, ";");
-								utvonal_tomb = concat(utvonal_tomb, buff);
-
-								
-
-								//return utvonal_tomb;
-							}
-						}
-					}
-					strcpy(utvonal_end, utvonal_tomb);//
+					itoa(iter->jaratszam, possible[j], 10);
+					j++;
 				}
 			}
 		}
 	}
+	
+	for (iter = lista; iter != NULL; iter = iter->kov)
+	{
+		for (i = 0; i < j; i++){
+			if (iter->jaratszam == atoi(possible[i])){
+				
+				if (melyseg >= 0)
+				{		
+//					buff = concat(parent, "-");
+					buff = concat(buff, possible[i]);
+					buff = concat(buff, "-");
+					buff = concat(buff, utvonal(lista, iter->repter2, hova, (melyseg - 1), buff));
+					temp_utvonal = concat(temp_utvonal, buff);
+					buff = "";
+				}
+				else
+				{
+					buff = concat(buff, "...");
+				//	temp_utvonal = concat(temp_utvonal, buff);
+					//temp_utvonal = concat(temp_utvonal, parent);
+					break;
+				}
+			}
+		}
+		
+	}
+
+	return temp_utvonal;
 }
-/*void utvonal(Data *lista, char *honnan, char *hova, char * utvonal_tomb[])
-{
-	Data *iter;
-	struct tm temptime;
-
-	int i=0;
-
-	for (iter = lista; iter != NULL; iter = iter->kov)
-	{
-		if (strcmp(kisbetusito(iter->repter1),kisbetusito(honnan))==0)
-		{
-			itoa(iter->jaratszam, utvonal_tomb[i], 10);
-			
-
-			if (strcmp(kisbetusito(iter->repter2), kisbetusito(hova)) == 0)
-			{
-				return utvonal_tomb[i];
-			}
-			i++;
-			
-		}
-	}
-
-	for (iter = lista; iter != NULL; iter = iter->kov)
-	{
-		for (i = 0; i < MAX; i++)
-		{
-			if (utvonal_tomb[i] == iter->jaratszam)
-			{
-				utvonal(iter, iter->repter1, hova, utvonal_tomb);
-			}
-		}
-	}
-}*/
 
 //Útvonaltervezés
 void utvonaltervezes(Data *lista){
 	char honnan[MAX];
 	char hova[MAX];
 	char seged[MAX];
+	char seged2[MAX];
 	int max_atszallasok = 10;
 	int *lvl;
 	Data *iter, *iter2;
 	char utvonal_tomb[MAX][MAX];
-	char utvonal_end[MAX][MAX];
+	char * utvonal_end = "";
 	int index = 0;
 	int end[MAX];
 	struct tm temptime;
 	char temp[MAX];
 	int i, j = 0;
+	char *running ="";
+	char *p,*q;
+
+	printf("Kerem adja meg az induló ország nevét: ");
+	getline(honnan, MAX - 1);
+	printf("Kerem adja meg az érkezõ ország nevét: ");
+	getline(hova, MAX - 1);
+	do{
+		printf("Kerem adja meg az átszállások maximális számát (hagyja üresen, ha nincs kikötés): ");
+		getline(seged, 9);	//Bekérjük a számot, addig amíg nem egész
+		if (egesze(seged) != 1 || seged == "")
+		{
+			printf("Hibás járatszám ! Kérem adja meg újra !\n\n");
+		}
+	} while (egesze(seged) != 1 || seged == "");
+	if (egesze(seged) == 1)
+	{
+		max_atszallasok = atoi(seged);
+	}
+	lvl = &max_atszallasok;
+
+	utvonal_end = concat(utvonal_end, utvonal(lista, honnan, hova, *lvl, ""));
+
+	running =( utvonal_end);
+	for (p = strsep(running, ";"); p != NULL; p = strsep(NULL, ";"))					
+	{
+		strcpy(seged, p);
+		strcpy(seged2, seged);
+		for (q = strsep(seged2, "-"); q != NULL; q = strsep(NULL, "-"))
+		{
+			strcpy(seged2, q);
+
+			printf("%s,", seged2);
+		}
+		printf("\n");
+		
+	}
+
+}*/
+
+
+graf * FillGraph(struct graf *Graf, Data *lista)
+{
+	Data *iter;
+	csucs csucsok[35];
+	int csucs_check = 1;
+	csucs csucs_buff;
+	csucs csucs_buff2;
+	csucs ellista[35][2];
+	el elek[MAX];
+	int i =0;
+	int j = 0;
+	int k = 0;
+
+
+	//Csúcs feltöltés
+	for (iter = lista; iter!= NULL; iter=iter->kov)
+	{
+		for (j = 0; j < i; j++)
+		{
+			if (strcmp(csucsok[j].allomas, iter->repter1) == 0)
+			{
+				csucs_check = 0;
+			}
+		}
+		if (csucs_check == 1)
+		{
+			csucsok[i].id = iter->jaratszam;
+			csucsok[i].allomas = iter->repter1;
+			csucsok[i].parent_id = NULL;
+			strcpy(csucsok[i].country, iter->repter1_country);
+			i++;
+		}
+		csucs_check = 1;
+	}
+
+	//Él feltöltés
+	i = 0;
+	for (iter = lista; iter != NULL; iter = iter->kov)
+	{
+		for (j = 0; j < 35; j++)
+		{
+			if (strcmp(csucsok[j].allomas, iter->repter1) == 0)
+			{
+				csucs_buff.id = csucsok[j].id;
+			}
+		}
+		csucs_buff.allomas = iter->repter1;
+		csucs_buff.parent_id = NULL;
+		strcpy(csucs_buff.country, iter->repter1_country);
+		elek[i].kezdet = csucs_buff;
+
+		for (j = 0; j < 35; j++)
+		{
+			if (strcmp(csucsok[j].allomas, iter->repter2) == 0)
+			{
+				csucs_buff.id = csucsok[j].id;
+			}
+		}
+		strcpy(csucs_buff.country, iter->repter2_country);
+		csucs_buff.allomas = iter->repter2;
+
+		elek[i].el_id = i;
+		elek[i].indulas = iter->indulas;
+		elek[i].erkezes = iter->erkezes;
+		elek[i].tavolsag = iter->tavolsag;
+		strcpy(elek[i].osztalyok, iter->osztalyok);
+		elek[i].idoeltolodas = iter->idoeltolodas;
+		elek[i].veg = csucs_buff;
+		i++;
+	}
+
+	//Szomszédos élek
+	j = 0;
+	for (i = 0; i < 35;i++)
+	{
+		for (iter = lista; iter != NULL; iter = iter->kov)
+		{
+			if (csucsok[i].allomas == iter->repter1)
+			{
+				ellista[j][0] = csucsok[i];
+
+				for (k = 0; k < 35; k++)
+				{
+					if (strcmp(csucsok[k].allomas, iter->repter2) == 0)
+					{
+						csucs_buff2.id = csucsok[k].id;
+					}
+				}
+
+				csucs_buff2.allomas = iter->repter2;
+				csucs_buff2.parent_id = NULL;
+				ellista[j][1] = csucs_buff2;
+				j++;
+			}
+		}
+	}
+
+	for (i = 0; i < 35; i++)
+	{
+		Graf->csucsok[i] = csucsok[i];
+		Graf->szomszedosCsucsok[i][0] = ellista[i][0];
+		Graf->szomszedosCsucsok[i][1] = ellista[i][1];
+
+	}
+
+	for (i = 0; i < MAX; i++)
+	{
+		Graf->elek[i] = elek[i];
+	}
+
+
+	
+
+	return Graf;
+}
+
+void Kerulo_kereses(struct graf *Graf, struct utvonal *ut_vonal[][MAX], char *honnan, char *hova, int lvl){
+	utvonal *iter;
+	int i;
+
+	printf(" ");
+	for (i = 0; i < MAX; i++)
+	{
+		for (iter = *ut_vonal[i]; iter != NULL; iter = iter->kov)
+		{
+
+		}
+	}
+}
+
+utvonal * FindPath(struct graf *Graf, char *honnan, char *hova, int lvl)
+{
+	utvonal *ut_vonal[MAX];
+	utvonal *buffer, *iter;
+	el *kozvetlen[MAX];
+
+	int indulo_csucsok_index = 0;
+	int kozvetlen_jarat_index = 0;
+	int i,j;
+	int first = 1;
+
+	for (i = 0; i < MAX; i++)
+	{
+		if (strcmp(kisbetusito(Graf->elek[i].kezdet.allomas),honnan)==0){
+
+			ut_vonal[indulo_csucsok_index] = (utvonal*)malloc(sizeof(utvonal));
+			
+			ut_vonal[indulo_csucsok_index]->allomas = Graf->elek[i].kezdet;
+			ut_vonal[indulo_csucsok_index]->el_id = Graf->elek[i].el_id;
+			ut_vonal[indulo_csucsok_index]->kov = NULL;
+			indulo_csucsok_index++;
+			
+		}
+	}
+
+	for (i = 0; i <= indulo_csucsok_index; i++)
+	{
+		for (buffer = ut_vonal[i];i<indulo_csucsok_index; buffer=ut_vonal[++i])
+		{
+			for (j = 0; j < MAX; j++)
+			{
+				if (buffer->el_id == Graf->elek[j].el_id)
+				{
+					if (strcmp(kisbetusito(Graf->elek[j].kezdet.allomas), kisbetusito(buffer->allomas.allomas)) == 0){
+						if (strcmp(kisbetusito(Graf->elek[j].veg.allomas), hova) == 0){
+							iter = (utvonal*)malloc(sizeof(utvonal));
+
+							iter->allomas = Graf->elek[j].veg;
+							iter->parent = buffer->allomas.id;
+							iter->el_id = -1;				//Nincs tovább = -1
+							ut_vonal[i]->kov = iter;
+							ut_vonal[i]->kov->kov = NULL;
+						}
+					}
+				}
+			}
+		}
+	}
+
+
+	//Kell még ID-ket egyeztetni ut_vonal-ban csúcsokat az élekhez.
+	for (i = 0; i < indulo_csucsok_index; i++)
+	{
+		buffer = ut_vonal[i];	
+		for (j = 0; j < MAX; j++)
+		{
+			if (buffer->kov != NULL)
+			{
+				if (Graf->elek[j].kezdet.id == buffer->allomas.id)
+				{
+					if (Graf->elek[j].veg.id == buffer->kov->allomas.id)
+					{
+						kozvetlen[kozvetlen_jarat_index] = (el*)malloc(sizeof(el));
+						*kozvetlen[kozvetlen_jarat_index] = Graf->elek[j];
+						kozvetlen_jarat_index++;
+					}
+				}
+			}
+		}
+	}
+
+	printf("\n%d közvetlen járatot találtam!\n\n", (kozvetlen_jarat_index));
+
+	for (i = 0; i < kozvetlen_jarat_index; i++)
+	{
+		printf("Honnan: %s (%s)\n", kozvetlen[i]->kezdet.allomas, kozvetlen[i]->kezdet.country);
+		printf("Hová: %s (%s)\n", kozvetlen[i]->veg.allomas, kozvetlen[i]->veg.country);
+		printf("Indulás: %d:%d\n", kozvetlen[i]->indulas.tm_hour, kozvetlen[i]->indulas.tm_min);
+		printf("Érkezés: %d:%d\n", kozvetlen[i]->erkezes.tm_hour, kozvetlen[i]->erkezes.tm_min);
+		printf("Idõeltolodás: GMT %s%d\n", (kozvetlen[i]->idoeltolodas>0 ? "+" : ""), kozvetlen[i]->idoeltolodas);
+		printf("Távolság: %.2f km\n", kozvetlen[i]->tavolsag);
+		printf("Osztályok: %s\n\n", kozvetlen[i]->osztalyok);
+	}
+
+	//Kerulo_kereses(Graf, ut_vonal, honnan, hova, --lvl);	
+	
+	return ut_vonal;
+}
+//Útvonaltervezés
+void utvonaltervezes(Data *lista){
+	char honnan[MAX];
+	int honnan_id;
+	int hova_id;
+	int i;
+	char hova[MAX];
+	char seged[MAX];
+	int max_atszallasok = 10;
+	int *lvl;
+	char * utvonal_end = "";
+	Data *iter;
+
+	graf *Graf = malloc((graf*)sizeof(graf));
+	
+
+
+	FillGraph(Graf, lista);
+
+	
 
 	printf("Kerem adja meg az induló ország nevét: ");
 	getline(honnan, MAX - 1);
@@ -675,32 +940,10 @@ void utvonaltervezes(Data *lista){
 	lvl = &max_atszallasok;
 
 
-	for (iter = lista; iter != NULL; iter = iter->kov)
-	{
-		if (strcmp(kisbetusito(honnan), kisbetusito(iter->repter1)) == 0){
-			itoa(iter->jaratszam, utvonal_tomb[index], 10);
-			index++;
-		}
-	}
+	FindPath(Graf, kisbetusito(honnan), kisbetusito(hova), *lvl);
 
-	for (i = 0; i < index; i++)
-	{
-		for (iter = lista; iter != NULL; iter = iter->kov)
-		{
-			if (atoi(utvonal_tomb[i]) == iter->jaratszam)
-			{
-				strcpy(utvonal_end[i], utvonal(lista, iter->repter2, hova, utvonal_tomb[i], lvl, utvonal_end));
-
-			}
-		}
-	}
-	for (j = 0; j < index; j++)
-	{
-		printf("%s;\n", utvonal_end[j]);
-	}
-
+	free(Graf);
 }
-
 //Fõprogram
 void main()
 {
